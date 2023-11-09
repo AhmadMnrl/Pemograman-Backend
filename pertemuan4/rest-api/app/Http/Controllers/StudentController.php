930e11c6ab51fe707b3742db510e1bdb1ce7b5ad
AhmadMnrl <serigalaberbulu465@gmail.com>
2023-11-09 20:22:12 +0700

    Tugas membuat RestFull-Api laravel pertemuan 6

--- pertemuan4/rest-api/app/Http/Controllers/StudentController.php
+++ pertemuan4/rest-api/app/Http/Controllers/StudentController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+
 class StudentController extends Controller
 {
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:sanctum')->except(['index', 'show']);
+    // }
     /**
      * Display a listing of the resource.
      *
@@ -14,28 +22,30 @@ class StudentController extends Controller
     public function index()
     {
         $students = Student::all();
-        if ($students) {
-            $data = [
-                "message" => "Get All Users",
-                "data" => $students
-            ];
-
-            return response()->json($data, 200);
-        } else {
-            return response()->json(["message" => "Error fetching students"], 500);
+        if ($students->isEmpty()) {
+            return response()->json(["message" => "Data Not found"], 404);
         }
+        $data = [
+            "message" => "Get All Users",
+            "data" => $students
+        ];
+        return response()->json($data, 200);
     }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        $input = [
-            'nama' => $request->nama,
-            'nim' => $request->nim,
-            'email' => $request->email,
-            'jurusan' => $request->jurusan
-        ];
-
-        $student = Student::create($input);
-
+        $validatedData = $request->validate([
+            'nama' => 'required',
+            'nim' => 'required',
+            'email' => 'required|email',
+            'jurusan' => 'required',
+        ]);
+        $student = Student::create($validatedData);
         if ($student) {
             $data = [
                 'message' => 'Student is created successfully',
@@ -47,54 +57,64 @@ class StudentController extends Controller
             return response()->json(["message" => "Error creating student"], 500);
         }
     }
-     /**
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
         $id = (int) $id;
-        $input = [
-            'nama' => $request->nama,
-            'nim' => $request->nim,
-            'email' => $request->email,
-            'jurusan' => $request->jurusan
-        ];
-        $student = Student::find($id)->update($input);
-
-        if ($student) {
-            $data = [
-                'message' => "Student is updated successfully",
-                'data' => $student
-            ];
-            return response()->json($data, 200);
-        } else {
-            return response()->json(["message" => "Error updating student"], 500);
+        $student = Student::find($id);
+        if (!$student) {
+            return response()->json(["message" => "Data Not Found"], 404);
         }
+        $validatedData = $request->validate([
+            'nama' => 'required',
+            'nim' => 'required',
+            'email' => 'required|email',
+            'jurusan' => 'required',
+        ]);
+        $student->update($validatedData);
+        $data = [
+            'message' => "Student is updated successfully",
+            'data' => $student
+        ];
+        return response()->json($data, 200);
     }
     /**
      * Remove the specified resource from storage.
      *
-     * @param  $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         $id = (int) $id;
-        $student = Student::destroy($id);
-
-        if ($student) {
-            $data = [
-                'message' => "Student is deleted successfully",
-                'data' => $student
-            ];
-            return response()->json($data, 200);
-        } else {
-            return response()->json(["message" => "Error deleting student"], 500);
+        $student = Student::find($id);
+        if (!$student) {
+            return response()->json(["message" => "Data Not Found"], 404);
         }
+        $student->delete();
+        $data = [
+            'message' => "Student is deleted successfully",
+            'data' => $student
+        ];
+        return response()->json($data, 200);
     }
-
+    public function show($id)
+    {
+        $id = (int) $id;
+        $student = Student::find($id);
+        if (!$student) {
+            return response()->json(["message" => "Data Not Found"], 404);
+        }
+        $data = [
+            "message" => "Get Student Detail",
+            "data" => $student
+        ];
+        return response()->json($data, 200);
+    }
 }
--- pertemuan4/rest-api/routes/api.php
+++ pertemuan4/rest-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,15 +20,18 @@ use App\Http\Controllers\StudentController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-// Route Animal
-Route::get('/animals',[AnimalController::class,'index']);
-Route::post('/animals', [AnimalController::class,'store']);
-Route::put('/animals/{id}', [AnimalController::class,'update']);
-Route::delete('/animals/{id}', [AnimalController::class,'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/animals',[AnimalController::class,'index']);
+    Route::post('/animals', [AnimalController::class,'store']);
+    Route::put('/animals/{id}', [AnimalController::class,'update']);
+    Route::delete('/animals/{id}', [AnimalController::class,'destroy']);
 
-Route::get('/students',[StudentController::class,'index']);
-Route::post('/students',[StudentController::class,'store']);
-Route::put('/students/{id}',[StudentController::class,'update']);
-Route::delete('/students/{id}',[StudentController::class,'destroy']);
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+    Route::get('/students/{id}', [StudentController::class, 'show']);
+});
 
-
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
