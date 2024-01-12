5d20b4b6d4800f627e97f01bb88acd62173b42b4
AhmadMnrl <serigalaberbulu465@gmail.com>
2024-01-12 19:42:36 +0700

    Task Pertemuan 13 add update,delete & show

--- pertemuan12/task/controllers/StudentController.js
+++ pertemuan12/task/controllers/StudentController.js
@@ -37,6 +37,96 @@ class StudentController {
             res.status(500).json({ error: "Internal Server Error" });
             }
     }
+    async update (req, res) {
+        try {
+          const { id } = req.params;
+      
+          const student = await Student.findByPk(id);
+      
+          if (student) {
+            const condition = {
+              where: {
+                id: id,
+              },
+            };
+      
+            const Student = await Student.update(req.body, condition);
+      
+            const data = {
+              message: "Mengedit data student",
+              data: Student,
+            };
+      
+            res.status(200).json(data);
+          } else {
+            const data = {
+              message: "Student not found",
+            };
+            res.status(404).json(data);
+          }
+        } catch (error) {
+          console.error("Error in update Student:", error);
+          res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+    async destroy(req, res) {
+        try {
+          const { id } = req.params;
+      
+          const student = await Student.findByPk(id);
+      
+          if (student) {
+            const condition = {
+              where: {
+                id: id,
+              },
+            };
+      
+            await Student.destroy(condition);
+      
+            const data = {
+              message: "Menghapus data student",
+            };
+      
+            res.status(200).json(data);
+          } else {
+            const data = {
+              message: "Student not found",
+            };
+            
+            return res.status(404).json(data);
+          }
+        } catch (error) {
+          console.error("Error in destroy:", error);
+          res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+    async show(req, res) {
+        try {
+          const { id } = req.params;
+      
+          const student = await Student.findByPk(id);
+      
+          if (student) {
+            const data = {
+              message: "Detail student",
+              data: student,
+            };
+      
+            res.status(200).json(data);
+          } else {
+            const data = {
+              message: "Student not found",
+            };
+      
+            res.status(404).json(data);
+          }
+        } catch (error) {
+          console.error("Error in show:", error);
+          res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+        
       
 }
 
--- pertemuan12/task/routes/api.js
+++ pertemuan12/task/routes/api.js
@@ -5,5 +5,8 @@ const router = express.Router()
 import StudentController from '../controllers/StudentController.js'
 router.get('/students',StudentController.index);
 router.post('/students',StudentController.store);
+router.put('/students/:id',StudentController.update);
+router.delete('/students/:id',StudentController.destroy);
+router.get('/students/:id',StudentController.show);
 
 export default router;
