688bfce21c620c2d84d660f09f19d4155302cbb4
AhmadMnrl <serigalaberbulu465@gmail.com>
2024-01-18 03:36:34 +0700

    praktikum 14 add to validasi,authentication,middleware,sort,filter,paging

--- pertemuan12/task/routes/api.js
+++ pertemuan12/task/routes/api.js
@@ -3,16 +3,20 @@ import StudentController from '../controllers/StudentController.js';
 import AuthController from '../controllers/AuthController.js';
 import auth from '../middleware/auth.js';
 
-const app = express();
+const router = express.Router()
 
-app.use('/students', auth);
+router.use((req, res, next) => {
+    res.status(404).json({ message: "Not Found" });
+});
 
-app.get('/students', StudentController.index);
-app.get('/students/:id', StudentController.show);
-app.post('/students', StudentController.store);
-app.put('/students/:id', StudentController.update);
-app.delete('/students/:id', StudentController.destroy);
+router.use('/students', auth);
 
-app.post('/register', AuthController.register);
-app.post('/login', AuthController.login);
-export default app;
+router.get('/students', StudentController.index);
+router.get('/students/:id', StudentController.show);
+router.post('/students', StudentController.store);
+router.put('/students/:id', StudentController.update);
+router.delete('/students/:id', StudentController.destroy);
+
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
+export default router;
